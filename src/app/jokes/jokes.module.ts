0278212af37307e7962517f6faf3f121e3b667ab
Olivier Schweitzer <olivier.sch68@gmail.com>
2018-02-10 17:06:08 +0100

    feat(jokes): Import used modules

--- src/app/jokes/jokes.module.ts
+++ src/app/jokes/jokes.module.ts
@@ -2,13 +2,18 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ChuckJokesComponent} from './chuck-jokes/chuck-jokes.component';
 import {DadJokesComponent} from './dad-jokes/dad-jokes.component';
-import {MatButtonModule, MatExpansionModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatExpansionModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatCardModule,
+    AngularFontAwesomeModule,
+    HttpClientModule
   ],
   exports: [
     DadJokesComponent,
