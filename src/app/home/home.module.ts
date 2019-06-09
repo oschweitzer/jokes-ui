d8fdbe23e114a6641a42df9ba78c4294ea5aec60
Oli1810 <olivier.sch68@gmail.com>
2019-06-09 14:40:42 +0200

    feat: Update to Angular8

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home/home.component';
 import {JokesModule} from '../jokes/jokes.module';
-import {MatDividerModule, MatExpansionModule, MatListModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
--- src/app/jokes/jokes.module.ts
+++ src/app/jokes/jokes.module.ts
@@ -2,7 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ChuckJokesComponent} from './chuck-jokes/chuck-jokes.component';
 import {DadJokesComponent} from './dad-jokes/dad-jokes.component';
-import {MatButtonModule, MatCardModule, MatExpansionModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 import {HttpClientModule} from '@angular/common/http';
 import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
